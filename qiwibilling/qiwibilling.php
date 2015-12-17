75e3d6dffd2c919aa88c65b830d93b0c30f2caba
devkont <yusupovb@gmail.com>
2015-12-17 21:51:50 +0300

    Amount format

--- qiwibilling/qiwibilling.php
+++ qiwibilling/qiwibilling.php
@@ -33,7 +33,7 @@ if (empty($m))
 
 		cot_block($pinfo['pay_status'] == 'new' || $pinfo['pay_status'] == 'process');
 		
-		$summ = $pinfo['pay_summ']*$cfg['plugin']['qiwibilling']['rate'];
+		$summ = number_format($pinfo['pay_summ']*$cfg['plugin']['qiwibilling']['rate'], 2);
 		
 		if($cfg['plugin']['qiwibilling']['protocol'] == 'rest')
 		{
